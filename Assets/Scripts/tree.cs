17e86c442c0a1f1273bc0ed0ee55eee5c5c3fef0
Taaun <sappakdee_t@silpakorn.edu>
2026-08-24 23:00:39 +0700

    lose HP

---
+++ Assets/Scripts/tree.cs
@@ -0,0 +1,31 @@
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class tree : MonoBehaviour
+{
+    private MeshRenderer rd;
+    void Start()
+    {
+        rd = GetComponent<MeshRenderer>();
+    }
+
+    void Update()
+    {
+        
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        rd.material.color = Color.red;
+
+        Player player = collision.gameObject.GetComponent<Player>();
+        if (player == null )
+            return;
+
+        player.Hp -= 15;
+    }
+
+    private void OnCollisionExit(Collision collision)
+    {
+        rd.material.color = new Color32(91 ,44 ,22 ,255);
+    }
+}
